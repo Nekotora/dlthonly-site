19e02c01bb0625a0ffd7d41f2560bda0f59c8613
NekoTora <i@flag.moe>
2018-07-29 18:26:35 +0800

    remove useless code, fix http over https, add console log

--- src/source/script/app.js
+++ src/source/script/app.js
@@ -8,4 +8,6 @@ $(function () {
   function setNav(name) {
     $('.nav #' + name).addClass('current');
   }
+  console.log("%c(｡･∀･)ﾉﾞNyan~ket！ご注文はねこですか？", "color:pink");
+  console.log("%cCreated by Nekotora.\nfind me at http://flag.moe/", "color:green");
 })
--- src/source/script/apply.js
+++ src/source/script/apply.js
@@ -1,5 +1,3 @@
 const $ = require('jquery');
 $(function () {
-  var ic = $('#wjform').iframe
-  console.log($('#wjform'))
 })
