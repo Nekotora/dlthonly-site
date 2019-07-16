4d920e0889ad7296d0b8d21b4de9f7b153536f62
Nekotora <i@flag.moe>
2019-07-17 01:36:08 +0800

    init ver 2020

--- src/source/script/app.js
+++ src/source/script/app.js
@@ -1,13 +1,5 @@
-const Parallax = require('parallax-js');
-const $ = require('jquery');
-const ionicons = require('ionicons');
-
 $(function () { 
-  var scene = document.getElementById('scene');
-  var parallaxInstance = new Parallax(scene);
-  function setNav(name) {
-    $('.nav #' + name).addClass('current');
-  }
+
   console.log("%c(｡･∀･)ﾉﾞNyan~ket！ご注文はねこですか？", "color:pink");
   console.log("%cCreated by Nekotora.\nfind me at http://flag.moe/", "color:green");
 })
