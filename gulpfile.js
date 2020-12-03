48063457fba7b47fc29c414ccff1234426068ecc
Nekotora <i@flag.moe>
2020-12-04 00:31:39 +0800

    upgrade gulp script

--- gulpfile.js
+++ gulpfile.js
@@ -1,75 +1,79 @@
-var gulp = require('gulp');
+var { series, src, dest, watch } = require('gulp');
 var preprocess = require('gulp-preprocess');
 var browserSync = require("browser-sync").create();
 var pug = require('gulp-pug');
 var del = require('del');
 var less = require('gulp-less');
 
-gulp.task('html', function() {
-  gulp.src('./src/*.pug')
-    .pipe(preprocess({ context: { curtime: Date.now() } }))
-    .pipe(pug())
-    .pipe(gulp.dest('./dist/'))
-});
+function html() {
+    return src('./src/*.pug')
+        .pipe(preprocess({ context: { curtime: Date.now() } }))
+        .pipe(pug())
+        .pipe(dest('./dist/'))
+}
 
-gulp.task('scripts', function() {
-  gulp.src(['./src/**/*.js'])
-    .pipe(preprocess())
-    .pipe(gulp.dest('./dist/'))
-});
+function scripts() {
+    return src(['./src/**/*.js'])
+        .pipe(preprocess())
+        .pipe(dest('./dist/'))
+}
 
-gulp.task('css', function() {
-  gulp.src('./src/**/*.css')
-    .pipe(preprocess({context: { NODE_ENV: 'production', DEBUG: true}}))
-    .pipe(gulp.dest('./dist/'))
-});
+function css() {
+    return src('./src/**/*.css')
+        .pipe(preprocess({ context: { NODE_ENV: 'production', DEBUG: true } }))
+        .pipe(dest('./dist/'))
+}
 
-gulp.task('less', function () {
-  return gulp.src('./src/**/*.less')
-    .pipe(less())
-    .pipe(gulp.dest('./dist/'));
-});
+function styleLess() {
+    return src('./src/**/*.less')
+        .pipe(less())
+        .pipe(dest('./dist/'));
+}
 
-gulp.task('static', function() {
-  gulp.src('./static/**/*.*')
-    .pipe(gulp.dest('./dist/'))
-});
+function staticFiles() {
+    return src('./static/**/*.*')
+        .pipe(dest('./dist/'))
+}
 
-gulp.task('vendor', function() {
-  gulp.src('./vendor/**/*.*')
-    .pipe(gulp.dest('./dist/'))
-});
+function vendor() {
+    return src('./vendor/**/*.*')
+        .pipe(dest('./dist/'))
+}
 
-gulp.task('serve', function(done) {
-  browserSync.init({
-    server: {
-      baseDir: './dist',
-      serveStaticOptions: {
-        extensions: ['html'] // pretty urls
-      }
-    },
-  });
-  done();
-})
+function serve() {
+    return browserSync.init({
+        server: {
+            baseDir: './dist',
+            serveStaticOptions: {
+                extensions: ['html'] // pretty urls
+            }
+        },
+    });
+}
 
-gulp.task('reload', function (done) {
-  setTimeout(function(){
-    browserSync.reload();
+function reload() {
+    return setTimeout(function() {
+        browserSync.reload();
+    }, 300)
+}
+
+function watchFiles(done) {
+    series(scripts, css, less, html, staticFiles, vendor)
+    watch('./src/**/*.js', scripts);
+    watch('./src/**/*.less', styleLess);
+    watch('./src/**/*.css', css);
+    watch('./src/**/*.pug', html);
+    watch('./static/**/*.*', staticFiles);
+    watch('./vendor/**/*.*', vendor);
+    watch('./dist/**/*.*', reload);
     done();
-  }, 300)
-});
+}
 
-gulp.task('dev', ['scripts', 'css', 'less', 'html', 'static','vendor', 'serve'], function(cb) {
-  gulp.watch('./src/**/*.js', ['scripts']);
-  gulp.watch('./src/**/*.less', ['less']);
-  gulp.watch('./src/**/*.css', ['css']);
-  gulp.watch('./src/**/*.pug', ['html']);
-  gulp.watch('./static/**/*.*', ['static']);
-  gulp.watch('./vendor/**/*.*', ['vendor']);
 
-  gulp.watch('./dist/**/*.*', ['reload']);
-});
+function clean() {
+    return del(['dist/*'], { dot: true })
+}
 
-gulp.task('clean', () => del(['dist/*'], { dot: true }));
-
-gulp.task('build', ['scripts', 'css', 'less', 'html', 'static', 'vendor'])
+exports.dev = series(watchFiles, serve)
+exports.clean = series(del)
+exports.build = series(clean, scripts, css, styleLess, html, staticFiles, vendor)
